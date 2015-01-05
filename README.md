3986cd305dd5fdc35e22948393f3b9982c1c22b9
Brandon Horst <brandonhorst@gmail.com>
2015-01-05 12:54:23 -0500

    initial implementation

--- README.md
+++ README.md
@@ -1,4 +1,31 @@
 lacona-addon-ordered
 ====================
 
-Add an ordering system to the lacona parser
+`lacona-addon-ordered` provides all of the same benefits of `lacona-addon-stateful` but also introduces the concept of ordering.
+
+Ordering is imposed on one particular text-based result. - a combination of the match, suggestion, and completion of a particular command. `lacona-addon-ordered` maintains that these two strings:
+
+- `*open* Firefox`
+- `*open* Google Chrome`
+
+will sort in the same order reliably, based upon a given computation function If the string `*open* Firefox Developer Edition` has been entered, that will not influence the sort order of `*open* Firefox`, or vice-versa.
+
+
+
+This information is stored in an object, which can be JSON-serialized for persistence.
+
+```js
+var lacona = require('lacona');
+var Stateful = require('lacona-addon-stateful');
+var Ordered = require('lacona-addon-ordered');
+
+var parser = new lacona.Parser();
+var stateful = new Stateful();
+var ordered = new Ordered({compare: myPredicate});
+
+src
+  .pipe(parser)
+  .pipe(stateful)
+  .pipe(ordered)
+  .pipe(process.stdout);
+```
---
+++ test/test.js
@@ -0,0 +1,149 @@
+var chai = require('chai');
+var stream = require('stream');
+var find = require('lodash.find');
+
+var lacona = require('lacona');
+var Stateful = require('lacona-addon-stateful');
+var Ordered = require('..');
+var fulltext = require('lacona-util-fulltext');
+
+var expect = chai.expect;
+
+var grammar = {
+  phrases: [{
+    name: 'test',
+    root: {
+      type: 'choice',
+      children: [
+        'testb',
+        'teaseaa',
+        'testbb'
+      ]
+    }
+  }]
+};
+
+function toStream(strings) {
+  var newStream = new stream.Readable({objectMode: true});
+
+  strings.forEach(function (string) {
+    newStream.push(string);
+  });
+  newStream.push(null);
+
+  return newStream;
+}
+
+function toArray(done) {
+  var newStream = new stream.Writable({objectMode: true});
+  var list = [];
+  newStream.write = function(obj) {
+    list.push(obj);
+  };
+
+  newStream.end = function() {
+    done(list);
+  };
+
+  return newStream;
+}
+
+
+describe('lacona-addon-ordered', function () {
+  var parser, stateful, ordered;
+
+  beforeEach(function () {
+    parser = new lacona.Parser({sentences: ['test']}).understand(grammar);
+    stateful = new Stateful({serializer: fulltext});
+
+    ordered = new Ordered({comparator: function (option) {
+      return fulltext(option).length;
+    }});
+  });
+
+  it('maintains order according to the given predicate' , function (done) {
+    function callback(data) {
+      expect(data).to.have.length(3);
+
+      //actual order is not changed
+      expect(data[0].data.suggestion.words[0].string).to.equal('testb');
+      expect(data[1].data.suggestion.words[0].string).to.equal('teaseaa');
+      expect(data[2].data.suggestion.words[0].string).to.equal('testbb');
+
+      //sortIndex is specified
+      expect(data[0].sortIndex).to.equal(0);
+      expect(data[1].sortIndex).to.equal(1);
+      expect(data[2].sortIndex).to.equal(1);
+      done();
+    }
+
+    toStream(['te'])
+      .pipe(parser)
+      .pipe(stateful)
+      .pipe(ordered)
+      .pipe(toArray(callback));
+  });
+
+  it('maintains order even with deletes' , function (done) {
+    function callback(data) {
+      expect(data).to.have.length(6);
+
+      //actual order is not changed
+      expect(data[0].data.suggestion.words[0].string).to.equal('testb');
+      expect(data[1].data.suggestion.words[0].string).to.equal('teaseaa');
+      expect(data[2].data.suggestion.words[0].string).to.equal('testbb');
+      expect(data[3].event).to.equal('update');
+      expect(data[3].data.suggestion.words[0].string).to.equal('testb');
+      expect(data[4].event).to.equal('update');
+      expect(data[4].data.suggestion.words[0].string).to.equal('testbb');
+      expect(data[5].event).to.equal('delete');
+
+      //sortIndex is specified
+      expect(data[0].sortIndex).to.equal(0);
+      expect(data[1].sortIndex).to.equal(1);
+      expect(data[2].sortIndex).to.equal(1);
+      expect(data[3].sortIndex).to.equal(0);
+      expect(data[4].sortIndex).to.equal(1);
+
+      done();
+    }
+
+    toStream(['te', 'tes'])
+    .pipe(parser)
+    .pipe(stateful)
+    .pipe(ordered)
+    .pipe(toArray(callback));
+  });
+
+  // it('does not emit multiple events if parsed before handling' , function (done) {
+  //   function callback(data) {
+  //     expect(data).to.have.length(2);
+  //     expect(data[0].event).to.equal('insert');
+  //     expect(data[1].event).to.equal('update');
+  //     expect(data[0].data.suggestion.words[0].string).to.equal('test');
+  //     expect(data[1].data.suggestion.words[0].string).to.equal('test');
+  //     done();
+  //   }
+  //
+  //   toStream(['t', 'te'])
+  //   .pipe(parser)
+  //   .pipe(stateful)
+  //   .pipe(toArray(callback));
+  // });
+  //
+  // it('does not emit events if none are valid' , function (done) {
+  //   function callback(data) {
+  //     expect(data).to.have.length(2);
+  //     expect(data[0].event).to.equal('insert');
+  //     expect(data[1].event).to.equal('delete');
+  //     expect(data[0].data.suggestion.words[0].string).to.equal('test');
+  //     done();
+  //   }
+  //
+  //   toStream(['t', 'tx'])
+  //   .pipe(parser)
+  //   .pipe(stateful)
+  //   .pipe(toArray(callback));
+  // });
+
+});
