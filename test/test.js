2a30f7b58597f664ac98db9a5995ae21af0abc27
Brandon Horst <brandonhorst@gmail.com>
2015-01-22 17:21:57 -0500

    Removed update functionality

--- test/test.js
+++ test/test.js
@@ -43,9 +43,9 @@ describe('lacona-addon-ordered', function () {
       name: 'test/test',
       describe: function () {
         return lacona.choice({children: [
-          lacona.literal({text: 'testb'}),
-          lacona.literal({text: 'teaseaa'}),
-          lacona.literal({text: 'testbb'})
+          lacona.literal({text: 'bb'}),
+          lacona.literal({text: 'aa'}),
+          lacona.literal({text: 'bc'})
         ]});
       }
     });
@@ -53,29 +53,32 @@ describe('lacona-addon-ordered', function () {
     parser = new lacona.Parser();
     parser.sentences = [test()];
     stateful = new Stateful({serializer: fulltext.all});
-
-    ordered = new Ordered({comparator: function (option) {
-      return fulltext.all(option).length;
-    }});
+    ordered = new Ordered({serializer: fulltext.all});
   });
 
   it('maintains order according to the given predicate' , function (done) {
     function callback(data) {
       expect(data).to.have.length(3);
 
-      //actual order is not changed
-      expect(fulltext.suggestion(data[0].data)).to.equal('testb');
-      expect(fulltext.suggestion(data[1].data)).to.equal('teaseaa');
-      expect(fulltext.suggestion(data[2].data)).to.equal('testbb');
-
-      //id is specified
+      expect(data[0].event).to.equal('insert');
       expect(data[0].id).to.equal(0);
+      expect(data[0].sortIndex).to.equal(0);
+      expect(fulltext.suggestion(data[0].data)).to.equal('bb');
+
+      expect(data[1].event).to.equal('insert');
       expect(data[1].id).to.equal(1);
-      expect(data[2].id).to.equal(1);
+      expect(data[1].sortIndex).to.equal(0);
+      expect(fulltext.suggestion(data[1].data)).to.equal('aa');
+
+      expect(data[2].event).to.equal('insert');
+      expect(data[2].id).to.equal(2);
+      expect(data[2].sortIndex).to.equal(2);
+      expect(fulltext.suggestion(data[2].data)).to.equal('bc');
+
       done();
     }
 
-    toStream(['te'])
+    toStream([''])
       .pipe(parser)
       .pipe(stateful)
       .pipe(ordered)
@@ -84,29 +87,32 @@ describe('lacona-addon-ordered', function () {
 
   it('maintains order even with deletes' , function (done) {
     function callback(data) {
-      expect(data).to.have.length(6);
+      expect(data).to.have.length(8);
 
       //actual order is not changed
-      expect(fulltext.suggestion(data[0].data)).to.equal('testb');
-      expect(fulltext.suggestion(data[1].data)).to.equal('teaseaa');
-      expect(fulltext.suggestion(data[2].data)).to.equal('testbb');
-      expect(data[3].event).to.equal('update');
-      expect(fulltext.suggestion(data[3].data)).to.equal('testb');
-      expect(data[4].event).to.equal('update');
-      expect(fulltext.suggestion(data[4].data)).to.equal('testbb');
-      expect(data[5].event).to.equal('delete');
-
-      //id is specified
-      expect(data[0].id).to.equal(0);
-      expect(data[1].id).to.equal(1);
-      expect(data[2].id).to.equal(1);
+      expect(data[3].event).to.equal('delete');
       expect(data[3].id).to.equal(0);
-      expect(data[4].id).to.equal(1);
+
+      expect(data[4].event).to.equal('insert');
+      expect(data[4].id).to.equal(3);
+      expect(data[4].sortIndex).to.equal(1);
+      expect(fulltext.suggestion(data[4].data)).to.equal('bb');
+
+      expect(data[5].event).to.equal('delete');
+      expect(data[5].id).to.equal(2);
+
+      expect(data[6].event).to.equal('insert');
+      expect(data[6].id).to.equal(4);
+      expect(data[6].sortIndex).to.equal(2);
+      expect(fulltext.suggestion(data[6].data)).to.equal('bc');
+
+      expect(data[7].event).to.equal('delete');
+      expect(data[7].id).to.equal(1);
 
       done();
     }
 
-    toStream(['te', 'tes'])
+    toStream(['', 'b'])
     .pipe(parser)
     .pipe(stateful)
     .pipe(ordered)
